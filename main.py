53499a4d1ec1e80e85bd355ae93dcd507494876a
pythongiant <srihari.unnikrishnan@gmail.com>
2017-05-27 10:23:59 +0530

    Init

---
+++ main.py
@@ -0,0 +1,38 @@
+"""
+Simple Artificial Intelligence Program
+SAIP
+
+"""
+import json
+net=open("assosiation.json","r+")
+
+existing=json.load(net)
+nounsFile=open("nouns.txt","r")
+nounsList=nounsFile.read().split("\n")
+nouns=[x.lower() for x in nounsList]
+occurence=0
+keywords=[]
+emotions=["good","bad","cool","gross","dirty","love","sad","dangerous","happy"]
+print("\t This is ASAIP-A Simple Artificial Intelligence Program\n")
+input_str=input("Please give a starting senctence for the conversation so i can learn some stuff ").split()
+
+for word in range(len(input_str)):
+	if input_str[word].lower() in nouns:
+		keywords.append(input_str[word])		
+
+association={}
+emotion_association={}
+
+for keyword in keywords:		
+	description=input("tell me more about "+keyword+"(s): ")
+	response=description.split()
+	for emotion in emotions:
+		if emotion in response:
+			emotion_association[keyword]=emotion
+
+	association[keyword]=description
+open('assosiation.json', 'w').close()
+association.update(emotion_association)
+association.update(existing)
+net=open("assosiation.json","a")
+net.write(json.dumps(association))
