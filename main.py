ef8cfa0873170a2cbe0da03c128c0f28901aa6f2
pythongiant <srihari.unnikrishnan@gmail.com>
2017-05-31 09:25:08 +0530

    This is now an assistant

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ Simple Artificial Intelligence Program
 SAIP
 
 """
-import json
-net=open("association.json","r+")
+import wolframalpha
 
-existing=json.load(net)
+client = wolframalpha.Client('5L4TR5-3QRQ88JWA3')
 nounsFile=open("nouns.txt","r")
 nounsList=nounsFile.read().split("\n")
 nouns=[x.lower() for x in nounsList]
@@ -14,25 +13,16 @@ occurence=0
 keywords=[]
 emotions=["good","bad","cool","gross","dirty","love","sad","dangerous","happy"]
 print("\t This is ASAIP-A Simple Artificial Intelligence Program\n")
-input_str=input("Please give a starting senctence for the conversation so i can learn some stuff ").split()
+input_str=raw_input("I am your personal assistant (for eg. how's the weather) ").split()
 
 for word in range(len(input_str)):
 	if input_str[word].lower() in nouns:
 		keywords.append(input_str[word])		
-
-association={}
-emotion_association={}
-
-for keyword in keywords:		
-	description=input("tell me more about "+keyword+"(s): ")
-	response=description.split()
-	for emotion in emotions:
-		if emotion in response:
-			emotion_association[keyword]=emotion
-
-	association[keyword]=description
-open('association.json', 'w').close()
-association.update(emotion_association)
-association.update(existing)
-net=open("association.json","a")
-net.write(json.dumps(association))
+print(str(len(keywords))+" keywords found")
+for word in keywords:
+	if_word=raw_input("do you want to know about "+word+" (YES)(NO)")
+	if if_word.lower() == "yes":
+		res = client.query(word)
+		for pod in res.pods:
+			for sub in pod.subpods:
+				print(sub.plaintext)
